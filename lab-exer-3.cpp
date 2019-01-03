c3808198b5a1b01c88bee66ead84632eb8bf1d49
Pamela Julia <46342286+pamelaisafish@users.noreply.github.com>
2019-01-03 18:32:07 +0800

    Add files via upload

---
+++ lab-exer-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	// lab-exer-3 question #2
+	
+	char word;
+	cout << "Enter a letter: ";
+	cin >> word;
+	cout << "The character code of " <<word<< " is " << int (word);
+	
+	
+	
+return 0;
+}
